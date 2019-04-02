495915dee44dc3713f14d7cd1e28eafa9a499ec4
unknown <rmsmall7@gmail.com>
2019-04-01 22:52:51 -0400

    initial creation

---
+++ key.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.spotify_ID,
+  secret: process.env.spotify_secret
+};
+
+
+
+
---
+++ liri.js
@@ -0,0 +1,9 @@
+var keys = require('./key.js');
+
+var spotify = require('spotify');
+
+var client = new spotify({
+  client_ID: '',
+  client_secret: '',
+});
+
